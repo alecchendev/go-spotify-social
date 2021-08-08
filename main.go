185f0ebe8610807d5fbb1bf09cda5feb29b61533
Alec Chen <alecchen@alecs-air.lan>
2021-08-08 09:06:37 -0500

    Setup serving dummy pages + path vars with gorilla/mux

--- main.go
+++ main.go
@@ -4,12 +4,42 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Spotify Social</h1>")
+}
+
+func profileHandler(w http.ResponseWriter, r *http.Request) {
+	pathVars := mux.Vars(r)
+	uri := pathVars["uri"]
+	fmt.Fprintf(w, "<h1>Profile: %v</h1>", uri)
+}
+
+func bookmarksHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Bookmarks</h1>")
+}
+
+func exploreHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Explore</h1>")
+}
+
+func settingsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Settings</h1>")
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>Spotify Social</h1>")
-	})
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/bookmarks", bookmarksHandler)
+	r.HandleFunc("/explore", exploreHandler)
+	r.HandleFunc("/settings", settingsHandler)
+	r.HandleFunc("/{uri}", profileHandler)
+	http.Handle("/", r)
+
 	port := ":8080"
 	fmt.Println("Serving at port", port)
 	log.Fatal(http.ListenAndServe(port, nil))
